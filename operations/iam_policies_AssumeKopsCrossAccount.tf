005dfc3b132230476ce68fcf97fbd44a2ce7b260
Michael Greer <michael.greer@bmwgroup.com>
2018-12-11 11:34:44 -0500

    removed logging cross account from operations as it is not needed.

--- operations/iam_policies_AssumeKopsCrossAccount.tf
+++ operations/iam_policies_AssumeKopsCrossAccount.tf
@@ -14,18 +14,3 @@ data "aws_iam_policy_document" "AssumeKopsCrossAccount" {
     ]
   }
 }
-
-data "aws_iam_policy_document" "allow_cross_account_application_logging" {
-  statement {
-    sid    = "IAMCrossAccountKinesisRole"
-    effect = "Allow"
-
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-    resources = [
-      "arn:aws:iam::${var.operations_aws_account_number}:role/*.${local.operation_cluster_name}",
-    ]
-  }
-}
